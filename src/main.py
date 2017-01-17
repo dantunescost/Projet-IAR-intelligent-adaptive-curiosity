a6c1decc456d6f5a17764c16d1ef69e3704f657f
BufferOverfl0w <cedric.hubert13@gmail.com>
2017-01-17 16:04:43 +0100

    squelette_sans_experts

--- src/main.py
+++ src/main.py
@@ -6,11 +6,15 @@
 
 import vrep
 import sys
+import numpy as np
+import random
 
 # declaration des variables
 DELAY = 150
+NB_ITERATIONS = 1500
 NS = 250
-
+LE = []	#liste erreur à chaque pas de temps
+LEm= [] #liste erreur moyenne à chaque pas de temps
 
 vrep.simxFinish(-1) # fermeture de toutes les connexions ouvertes
 clientID = vrep.simxStart('127.0.0.1',19999,True,True,5000,5) #etablissement de la connexion avec V-REP
@@ -45,10 +49,58 @@ def execute_action(cID, leftHandle, rightHandle, (speed_left,speed_right,frequen
     
     
     
+def bouclePrincipale():
+	t=0
+	possibleActions = []	#liste d'actions possibles à ce step
+    LPActions= []			#learning progress calculé pour chaque action
+
+	while t < NB_ITERATIONS:
+		'''
+			Selection de l'action
+		'''
+		for i in range(len(LPActions)):
+		
+			Ep = MetaPredictionMP(possibleActions[i]) #calcul de la prediction de l'erreur
+			tempLE = liste(LE) #on clone LE
+			tempLE.append(Ep) #on rajoute à la liste clonée l'erreur prédite
+			Emp= np.mean(tempLE[-DELAY:])
+			LP= -(-Emp-LEm[t+1-DELAY])
+			LPActions.append(LP)
+		
+		indiceActionChoisie = 0
+		if(random.random() > 0.1):			#exploitation
+			indiceActionChoisie = np.argmax(LPactions)
+		else:								#exploration
+			indiceActionChoisie = 0
+
+		'''
+			Prediction de la machine P
+		'''
+			S = PredictionP(possibleActions[indiceActionChoisie])
+		'''
+			Réalisation de l'action dans le simulateur
+		'''
+		#TODO
+
+		#On stoppe le robot
+		vrep.simxSetJointTargetVelocity(clientID,leftMotor,0,vrep.simx_opmode_oneshot)
+		vrep.simxSetJointTargetVelocity(clientID,rightMotor,0,vrep.simx_opmode_oneshot)
+		t += 1
+		'''
+			Vérification résultat action
+		'''
+		#TODO calcul de la distance, ce serait bien de faire avec senseurs, sinon directement avec les fonctions de VREPs
+		Sa=0
+		#calcul de l'erreur
+		E= abs(S-Sa)
+		#maj listes
+		LE.append(E)
+		Em= np.mean(LE[-DELAY:])
+		LEm.append(Em)
     
-    
-    
-    
-    
-    
+def MetaPredictionMP(action):
+	#TODO
+
+def PredictionP(action):
+	#TODO
     
