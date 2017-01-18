723336eac90d562eb3a7c3042f02277f105e6f63
Antunes Costa Goncalves Daniel <dantunescost92@gmail.com>
2017-01-18 16:59:02 +0100

    corrections et TODOs

--- src/main.py
+++ src/main.py
@@ -18,8 +18,8 @@ K = 5
 # declaration des variables
 LE = []         #liste erreur à chaque pas de temps
 LEm= []         #liste erreur moyenne à chaque pas de temps
-data_MP = []	#de la forme [param1,param2,param3,ecart]
-data_P = []	#de la forme [param1,param2,param3,distance]
+data_MP = []    #de la forme [param1,param2,param3,ecart]
+data_P = [] #de la forme [param1,param2,param3,distance]
 
 
 vrep.simxFinish(-1) # fermeture de toutes les connexions ouvertes
@@ -40,10 +40,10 @@ returnCode, balle = vrep.simxGetObjectHandle(clientID,"balle", vrep.simx_opmode_
 
 returnVal = vrep.simxCallScriptFunction(clientID,"",)
 
-def execute_action(cID, leftHandle, rightHandle, (speed_left,speed_right,frequency), botHandle, ballHandle):
-    vrep.simxSetJointTargetVelocity(cID,leftHandle,speed_left,vrep.simx_opmode_oneshot)
-    vrep.simxSetJointTargetVelocity(cID,rightHandle,speed_right,vrep.simx_opmode_oneshot)
-    if frequency > 0.66 and frequency <= 1:
+def execute_action(cID, leftHandle, rightHandle, action, botHandle, ballHandle):
+    vrep.simxSetJointTargetVelocity(cID,leftHandle,action[0],vrep.simx_opmode_oneshot)
+    vrep.simxSetJointTargetVelocity(cID,rightHandle,action[1],vrep.simx_opmode_oneshot)
+    if action[2] > 0.66 and action[2] <= 1:
         # ball jumps to robot
         returnCode1, pos1 = vrep.simxGetObjectPosition(cID,botHandle,-1,vrep.simx_opmode_oneshot_wait)
         returnCode2, pos2 = vrep.simxGetObjectPosition(cID,ballHandle,-1,vrep.simx_opmode_oneshot_wait)
@@ -56,7 +56,7 @@ def execute_action(cID, leftHandle, rightHandle, (speed_left,speed_right,frequen
 #        else: # balle plus loin sur l'axe x
 #            ratio = np.abs(pos1[1]-pos2[1])/np.abs(pos1[0]-pos2[0]) if np.abs(pos1[0]-pos2[0]) != 0 else 1
 #            vrep.simxSetObjectPosition(cID,ballHandle,botHandle,[largeurRobot*signeX,largeurRobot*signeY*ratio,0],vrep.simx_opmode_oneshot)
-    if frequency >= 0 and frequency <= 0.33:
+    if action[2] >= 0 and action[2] <= 0.33:
         # ball goes to random position
         returnCode1, pos1 = vrep.simxGetObjectPosition(cID,botHandle,-1,vrep.simx_opmode_oneshot_wait)
         returnCode2, pos2 = vrep.simxGetObjectPosition(cID,ballHandle,-1,vrep.simx_opmode_oneshot_wait)
@@ -78,98 +78,125 @@ def distance(cID):
     
     
 def bouclePrincipale():
-	t=0
-	possibleActions = []	#liste d'actions possibles à ce step
-	LPActions= []		#learning progress calculé pour chaque action
+    t=0
 
-	while t < NB_ITERATIONS:
-		'''
-			Génération liste d'actions possibles
-		'''
-		for i in range(NB_ACTIONS_ECHANTILLONAGE):
-			possibleActions.append( [random.uniform(-1,1) , random.uniform(-1,1) , random.uniform(0,1)] )
-		'''
-			Selection de l'action
-		'''
-		for i in range(len(possibleActions)):
-		
-			Ep = MetaPredictionMP(possibleActions[i]) #calcul de la prediction de l'erreur
-			tempLE = list(LE) #on clone LE
-			tempLE.append(Ep) #on rajoute à la liste clonée l'erreur prédite
-			Emp= np.mean(tempLE[-DELAY:]) #TODO calculer la moyenne des exemples existants s'il n'y en a pas encore DELAY
-			LP= -(-Emp-LEm[t+1-DELAY])
-			LPActions.append(LP)
-		
-		if(random.random() > 0.1):			#exploitation
-			indiceActionChoisie = np.argmax(LPActions)
-		else:								#exploration
-			indiceActionChoisie = 0
+    while t < NB_ITERATIONS:
+        possibleActions = []    #liste d'actions possibles à ce step
+        LPActions= []           #learning progress calculé pour chaque action
+        '''
+            Génération liste d'actions possibles
+        '''
+        for i in range(NB_ACTIONS_ECHANTILLONAGE):
+            possibleActions.append( [random.uniform(-1,1) , random.uniform(-1,1) , random.uniform(0,1)] )
+        '''
+            Selection de l'action
+        '''
+        for i in range(len(possibleActions)):
+        
+            Ep = MetaPredictionMP(possibleActions[i]) #calcul de la prediction de l'erreur
+            tempLE = list(LE) #on clone LE
+            tempLE.append(Ep) #on rajoute à la liste clonée l'erreur prédite
+            if t == 0:
+                LP = Ep
+            if t != 0 and t < DELAY: 
+                Emp = np.mean(tempLE)
+                LP = -(Emp-LEm[0])
+            else:
+                Emp= np.mean(tempLE[-DELAY:]) 
+                LP = -(Emp-LEm[t+1-DELAY])
+            LPActions.append(LP)
+        
+        if(random.random() > 0.1):          #exploitation
+            indiceActionChoisie = np.argmax(LPActions)
+        else:                               #exploration
+            indiceActionChoisie = 0
 
-		'''
-			Prediction de la machine P
-		'''
-		S = PredictionP(possibleActions[indiceActionChoisie])
-		'''
-			Réalisation de l'action dans le simulateur
-		'''
-		#TODO executer l'action dans le simulateur
+        '''
+            Prediction de la machine P
+        '''
+        S = PredictionP(possibleActions[indiceActionChoisie])
+        
+        '''
+            Réalisation de l'action dans le simulateur
+        '''
+        execute_action(clientID,leftMotor,rightMotor,possibleActions[indiceActionChoisie],robotHandle,balle)
 
-		#On stoppe le robot
-#		vrep.simxSetJointTargetVelocity(clientID,leftMotor,0,vrep.simx_opmode_oneshot_wait)
-#		vrep.simxSetJointTargetVelocity(clientID,rightMotor,0,vrep.simx_opmode_oneshot_wait)
-		t += 1
-		'''
-			Vérification résultat action
-		'''
-		# calcul de la distance, ce serait bien de faire avec senseurs, sinon directement avec les fonctions de VREP
-		Sa = distance(clientID)
-		#sauvegarde dans data_P
-		ajoutData=list(possibleActions[indiceActionChoisie])
-		ajoutData.append(Sa)
-		data_P.append(ajoutData)
-		#calcul de l'erreur
-		E = abs(S-Sa)
-		#sauvegarde dans data_MP
-		ajoutData=list(possibleActions[indiceActionChoisie])
-		ajoutData.append(E) #a verfifier
-		data_MP.append(ajoutData)
-		#maj listes
-		LE.append(E) 
-		Em = np.mean(LE[-DELAY:]) #TODO que si t > DELAY
-		LEm.append(Em)
-	return 0
+        #On stoppe le robot
+#       vrep.simxSetJointTargetVelocity(clientID,leftMotor,0,vrep.simx_opmode_oneshot_wait)
+#       vrep.simxSetJointTargetVelocity(clientID,rightMotor,0,vrep.simx_opmode_oneshot_wait)
+        t += 1
+        '''
+            Vérification résultat action
+        '''
+        # calcul de la distance, ce serait bien de faire avec senseurs, sinon directement avec les fonctions de VREP
+        Sa = distance(clientID)
+        #sauvegarde dans data_P
+        ajoutData=list(possibleActions[indiceActionChoisie])
+        ajoutData.append(Sa)
+        data_P.append(ajoutData)
+        #calcul de l'erreur
+        E = abs(S-Sa)
+        #sauvegarde dans data_MP
+        ajoutData=list(possibleActions[indiceActionChoisie])
+        ajoutData.append(E) #a verfifier
+        data_MP.append(ajoutData)
+        #maj listes
+        LE.append(E) 
+        if len(LE) < DELAY:
+            Em = np.mean(LE)
+        else: 
+            Em = np.mean(LE[-DELAY:]) 
+        LEm.append(Em)
+        
+    return 0
 
-#TODO pour les deux algos k-moyenne, renvoyer la moyenne des existants au lieu de 0 dans le cas ou len(data) < K ???
 def MetaPredictionMP(action):
-	d=[]	#on va ranger dans cette liste l'écart entre notre action et chaque exemple de la bdd
-	res=0	#valeur moyenne des K plus proches voisins, à retourner
-	for i in range(len(data_MP)):
-		d1=abs(data_MP[i][0]-action[0])
-		d2=abs(data_MP[i][1]-action[1])
-		d3=abs(data_MP[i][2]-action[2])
-		dtot=d1+d2+d3
-		d.append([dtot,i])
-	d.sort()	#on trie dans l'ordre croissant des écarts
-	if K <= len(data_MP):
-		for i in range(K):
-			res += data_MP[i][3]
-		res = res / K
-	return res
+    d=[]    #on va ranger dans cette liste l'écart entre notre action et chaque exemple de la bdd
+    res=0   #valeur moyenne des K plus proches voisins, à retourner
+    if len(data_MP) == 0:
+        return res
+    if len(data_MP) < K:
+        for i in range(len(data_MP)):
+            res += data_MP[i][3]
+        res = res / len(data_MP)
+    else:
+        for i in range(len(data_MP)):
+            d1=abs(data_MP[i][0]-action[0])
+            d2=abs(data_MP[i][1]-action[1])
+            d3=abs(data_MP[i][2]-action[2])
+            dtot=d1+d2+d3
+            d.append([dtot,i])
+        d.sort()    #on trie dans l'ordre croissant des écarts
+        for i in range(K):
+            res += data_MP[i][3]
+        res = res / K
+    return res
 
 def PredictionP(action):
-	d=[]	#on va ranger dans cette liste l'écart entre notre action et chaque exemple de la bdd
-	res=0	#valeur moyenne des K plus proches voisins, à retourner
-	for i in range(len(data_P)):
-		d1=abs(data_P[i][0]-action[0])
-		d2=abs(data_P[i][1]-action[1])
-		d3=abs(data_P[i][2]-action[2])
-		dtot=d1+d2+d3
-		d.append([dtot,i])
-	d.sort()	#on trie dans l'ordre croissant des écarts
-	if K <= len(data_P):
-		for i in range(K):
-			res += data_P[i][3]
-		res = res / K
-	
-	return res
+    d=[]    #on va ranger dans cette liste l'écart entre notre action et chaque exemple de la bdd
+    res=0   #valeur moyenne des K plus proches voisins, à retourner
+    if len(data_P) == 0:
+        return res
+    if len(data_P) < K:
+        for i in range(len(data_P)):
+            res += data_P[i][3]
+        res = res / len(data_P)
+    else:
+        for i in range(len(data_P)):
+            d1=abs(data_P[i][0]-action[0])
+            d2=abs(data_P[i][1]-action[1])
+            d3=abs(data_P[i][2]-action[2])
+            dtot=d1+d2+d3
+            d.append([dtot,i])
+        d.sort()    #on trie dans l'ordre croissant des écarts
+        if K <= len(data_P):
+            for i in range(K):
+                res += data_P[i][3]
+            res = res / K
     
+    return res
+    
+bouclePrincipale()
+    
+
+            
